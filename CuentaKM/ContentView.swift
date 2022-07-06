5b64d68b4cb47aec016e1f00d20e2a6b07367b56
Sergio Rodríguez Rama <ram.rod.ser+github@gmail.com>
2022-07-06 16:48:13 +0200

    :lipstick: kmh counter
    Dynamic types and error messages.

--- CuentaKM/ContentView.swift
+++ CuentaKM/ContentView.swift
@@ -19,23 +19,37 @@ struct ContentView: View {
         VStack {
             switch self.locationManager.authorizationStatus {
             case .authorizedAlways, .authorizedWhenInUse:
-                Text("\(self.locationManager.speed)")
+                VStack {
+                    Text("\(self.locationManager.speed, specifier: "%.1f")")
+                        .font(.system(size: 150, weight: .bold))
+                    Text("km/h").font(.largeTitle)
+                }
+                .lineLimit(1)
             case .denied:
-                Text("Denied")
+                Text("LOCATION AUTH STATUS\n\nUser has explicitly denied authorization for this application, or location services are disabled in Settings.")
             case .notDetermined:
-                Text("Not determined")
+                Text("LOCATION AUTH STATUS\n\nUser has not yet made a choice with regards to this application.")
             case .restricted:
-                Text("Restricted")
+                Text("LOCATION AUTH STATUS\n\nThis application is not authorized to use location services. Due to active restrictions on location services, the user cannot change this status, and may not have personally denied authorization")
             @unknown default:
-                Text("Unexpected authorization status: \(self.locationManager.authorizationStatus.rawValue)")
+                Text("LOCATION AUTH STATUS: \(self.locationManager.authorizationStatus.rawValue)")
             }
         }
+        .padding(.horizontal)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.black)
+        .foregroundColor(.white)
+        .minimumScaleFactor(0.01)
+        .multilineTextAlignment(.center)
         .onAppear { self.locationManager.requestAlwaysAuthorization() }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(locationManager: .init(locationManager: MockCLLocationManager(authorizationStatus: .authorizedAlways)))
+        ContentView(locationManager: .init(
+            locationManager: MockCLLocationManager(authorizationStatus: .authorizedAlways),
+            speed: 100000000
+        ))
     }
 }
